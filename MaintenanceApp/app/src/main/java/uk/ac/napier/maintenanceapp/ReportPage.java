2f77e5504f9cca925c23c43d958d372e604467a5
stechr8 <35330034+stechr8@users.noreply.github.com>
2018-02-07 23:12:23 +0000

    Dropdown visual

--- MaintenanceApp/app/src/main/java/uk/ac/napier/maintenanceapp/ReportPage.java
+++ MaintenanceApp/app/src/main/java/uk/ac/napier/maintenanceapp/ReportPage.java
@@ -3,7 +3,9 @@ package uk.ac.napier.maintenanceapp;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import static android.R.attr.startYear;
 
@@ -13,6 +15,11 @@ public class ReportPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_page);
+
+        Spinner spnHome = (Spinner)findViewById(R.id.spnHome);
+        String[] homes = new String[]{"Viewpark", "Abercorn", "Spring Gardens"};
+        ArrayAdapter<String> homeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, homes);
+        spnHome.setAdapter(homeAdapter);
 
         DatePicker dteDateSubmitted = (DatePicker)findViewById(R.id.dteDateSubmitted);
 
