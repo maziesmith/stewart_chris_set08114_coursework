1f0759bfa5700bab5ce105659f62cf8de239a5bf
stechr8 <35330034+stechr8@users.noreply.github.com>
2018-02-08 14:46:06 +0000

    Spinner priority

--- MaintenanceApp/app/src/main/java/uk/ac/napier/maintenanceapp/ReportPage.java
+++ MaintenanceApp/app/src/main/java/uk/ac/napier/maintenanceapp/ReportPage.java
@@ -21,7 +21,14 @@ public class ReportPage extends AppCompatActivity {
         ArrayAdapter<String> homeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, homes);
         spnHome.setAdapter(homeAdapter);
 
+        Spinner spnPriority = (Spinner)findViewById(R.id.spnPriority);
+        String[] priorities = new String[]{"Low", "Medium", "High", "Urgent"};
+        ArrayAdapter<String> prioritiesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorities);
+        spnPriority.setAdapter(prioritiesAdapter);
+        spnPriority.setSelection(prioritiesAdapter.getPosition("Medium"));
+
         DatePicker dteDateSubmitted = (DatePicker)findViewById(R.id.dteDateSubmitted);
+
 
     }
 }
